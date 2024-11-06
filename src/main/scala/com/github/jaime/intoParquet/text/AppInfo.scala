4adc08c85f095ad77eb82e2f14053c3533dfce29
Jaime Álvarez Fernández <jaime.alv.fdz@gmail.com>
2024-11-06 09:17:07 +0100

    Update --help message

--- src/main/scala/com/github/jaime/intoParquet/text/AppInfo.scala
+++ src/main/scala/com/github/jaime/intoParquet/text/AppInfo.scala
@@ -10,7 +10,8 @@ object AppInfo {
 
     private val version: String = readVersion
     private val PacketDescription: String = {
-        "Converts csv format files into parquet files, and can apply a schema when transforming them."
+        """Converts csv format files into parquet files,
+          |and can apply a schema when transforming them.""".stripMargin
     }
 
     private def readVersion: String = {
@@ -24,7 +25,8 @@ object AppInfo {
            |$PacketDescription
            |
            |Copyright (C) 2024 Free Software Foundation, Inc.
-           |License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+           |License GPLv3+: GNU GPL version 3 or later
+           |<https://gnu.org/licenses/gpl.html>.
            |This is free software: you are free to change and redistribute it.
            |There is NO WARRANTY, to the extent permitted by law.
            |
@@ -34,20 +36,32 @@ object AppInfo {
     val Example: String =
         s"""
           |[How to]
+          |This command-line tool is designed to convert CSV files into
+          |the efficient parquet format.
+          |
           |Create a directory structure as follows:
           |  ./data/input/
-          |Script will automatically create output folder inside ./data/
-          |Add csv files inside ./data/input/ together with a text file with the same name that contains the schema.
-          |Execute the app within the shell
-          |java -jar target/into-parquet-$version-jar-with-dependencies.jar
-          |Parquet files will appear inside ./data/output/ directory with the same name as the csv file
+          |Script will automatically create output data folder inside ./data/
+          |
+          |Add CSV files inside ./data/input/ together with a text file with
+          |the same name that contains the schema.
+          |  ./data/input/example.csv
+          |  ./data/input/example
+          |
+          |Schema files follows the convention: field_name field_type
+          |
+          |Execute the app within the shell:
+          |java -jar into-parquet-cli.jar
+          |
+          |Parquet files will appear inside ./data/output/ directory with the
+          |same name as the csv file.
           |
           |Default options:
-          | > Read all csv files found
-          | > Input path ./data/input/
-          | > Output path ./data/output/
-          | > Cast method parse schema
-          | > Fallback method None
-          | > Fail-fast set to false
+          | >> Read all csv files found inside input path that have a companion schema
+          | >> Input path ./data/input/
+          | >> Output path ./data/output/
+          | >> Cast method parse schema
+          | >> Fallback method None
+          | >> Fail-fast set to false
           |""".stripMargin
 }
